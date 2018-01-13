06a156f4da9d89a88e6e896bea8df841a65670f9
yangyuquan <254543778@qq.com>
2018-01-14 00:40:38 +0800

    登录/注册/忘记密码

--- CitySee.Client/src/components/SearchBarPlaceholder.js
+++ CitySee.Client/src/components/SearchBarPlaceholder.js
@@ -1,30 +1,34 @@
 import React, {Component} from 'react'
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+//import Icon from 'react-native-vector-icons/Ionicons'
+
 
 const styles = StyleSheet.create({
-    container:{
-        height:30,
-        display:'flex',
-        flexDirection:'row',
+    container: {
+        height: 30,
+        display: 'flex',
+        flexDirection: 'row',
         borderRadius: 24,
-        backgroundColor:'white',
-        justifyContent:'center',
-        alignItems:'center'
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    placeholder:{
-        color:'rgba(0,0,0,0.3)',
+    placeholder: {
+        color: 'rgba(0,0,0,0.3)',
         marginLeft: 5
     }
 })
 
-class SearchBarPlaceholder extends Component{
+const navLeft = require('../images/navLeft.png');
 
-    render(){
-        return(
+class SearchBarPlaceholder extends Component {
+
+    render() {
+        return (
             <TouchableOpacity activeOpacity={0.8} onPress={this.props.onClick}>
                 <View style={[styles.container, this.props.style]}>
-                    <Icon size={20} name="ios-search" color="rgba(0,0,0,0.3)"/>
+                    {/* <Icon size={20} name="ios-search" color="rgba(0,0,0,0.3)"/> */}
+                    <Image source={navLeft} />
                     <Text style={[styles.placeholder, this.props.textStyle]}>{this.props.placeholder}</Text>
                 </View>
             </TouchableOpacity>
